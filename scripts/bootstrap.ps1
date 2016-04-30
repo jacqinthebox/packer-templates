0ebd33531f03bfaede8fc509c0c790bff09e58f9
Jacqueline <jp@azure-architects.nl>
2016-04-30 08:16:22 +0100

    the files

---
+++ scripts/bootstrap.ps1
@@ -0,0 +1,59 @@
+# You cannot enable Windows PowerShell Remoting on network connections that are set to Public
+# Spin through all the network locations and if they are set to Public, set them to Private
+# using the INetwork interface:
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa370750(v=vs.85).aspx
+# For more info, see:
+# http://blogs.msdn.com/b/powershell/archive/2009/04/03/setting-network-location-to-private.aspx
+
+# Network location feature was only introduced in Windows Vista - no need to bother with this
+# if the operating system is older than Vista
+if([environment]::OSVersion.version.Major -lt 6) { return }
+
+# You cannot change the network location if you are joined to a domain, so abort
+if(1,3,4,5 -contains (Get-WmiObject win32_computersystem).DomainRole) { return }
+
+# Before anything else create the log output folder
+New-Item C:\Windows\Panther\Unattend -Type Directory
+New-Item c:\Logs -Type Directory
+Copy-Item a:\unattend.xml C:\Windows\Panther\Unattend\
+
+#disable LUA
+#New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+
+# Get network connections
+$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}'))
+$connections = $networkListManager.GetNetworkConnections()
+
+$connections |foreach {
+    #Write-Host $_.GetNetwork().GetName()'category was previously set to'$_.GetNetwork().GetCategory()
+    $_.GetNetwork().GetName() + 'category was previously set to' + $_.GetNetwork().GetCategory() | Out-File c:\logs\logfile.txt
+
+    $_.GetNetwork().SetCategory(1)
+
+    # Write-Host $_.GetNetwork().GetName()"changed to category"$_.GetNetwork().GetCategory()
+    $_.GetNetwork().GetName() + 'change to ' + $_.GetNetwork().GetCategory() | Out-File C:\Logs\logfile.txt -Append
+
+
+}
+
+function Enable-WinRM {
+netsh advfirewall firewall set rule group="remote administration" new enable=yes
+netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985
+
+winrm quickconfig -q
+winrm quickconfig -transport:http
+winrm set winrm/config '@{MaxTimeoutms="7200000"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
+winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
+winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/client/auth '@{Basic="true"}'
+
+net stop winrm
+sc.exe config winrm start= auto
+net start winrm
+ 
+}
+
+Enable-WinRM
---
+++ scripts/provision-core.ps1
@@ -0,0 +1,32 @@
+
+$secpasswd = ConvertTo-SecureString 'vagrant' -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ('vagrant', $secpasswd)
+
+Set-ExecutionPolicy Bypass -Force
+$WinlogonPath = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Remove-ItemProperty -Path $WinlogonPath -Name AutoAdminLogon
+Remove-ItemProperty -Path $WinlogonPath -Name DefaultUserName
+
+#enable RDP
+#'Enabling RDP' | Out-File c:\logs\logfile.txt -append
+#Write-Host 'Enabling RDP'
+#netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
+#reg add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server' /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+Import-Module PackageManagement -Force
+Get-PackageSource -Force -ForceBootstrap -Provider chocolatey 
+
+'Installing Guest Additions' | Out-File c:\logs\logfile.txt -append
+Write-Host 'Installing Guest Additions'
+& cmd /c certutil -addstore -f 'TrustedPublisher' A:\oracle-cert.cer
+& E:\VBoxWindowsAdditions.exe /S
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\Setup\Status\SysprepStatus'  -Name  'GeneralizationState' -Value 7
+
+Write-Host 'Sdelete things'
+
+Copy-Item a:\sdelete.exe c:\logs
+& cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
+& cmd /c C:\logs\sdelete.exe -q -z C:
+
+Write-Host 'Done'
