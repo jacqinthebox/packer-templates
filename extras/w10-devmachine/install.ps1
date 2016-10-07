a4c9293a83472ab0486d52aefa71089ce3803dcb
jacqueline <jacqueline@silverslank10>
2016-10-07 09:16:29 +0200

    extras

---
+++ extras/w10-devmachine/install.ps1
@@ -0,0 +1,11 @@
+New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
+New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 1
+Install-Module ISESteroids -Scope CurrentUser
+Install-Module PsISEProjectExplorer
+Import-Module PsISEProjectExplorer
+Add-PsISEProjectExplorerToIseProfile
+
+choco install git --yes --force
+choco install git-credential-winstore --yes --force
+choco install visualstudiocode --yes --force
+choco install VisualStudio2015Community --yes --force --packageParameters "--AdminFile https://gist.githubusercontent.com/jacqinthebox/494111cc5e2941e963d1424d15813cf2/raw/2a23194c41d866a7cd8ad8d68efe8a82595dfa70/AdminDeployment.xml"
