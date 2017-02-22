4f1270a5914af4c2c27b70edfa8ec68c93f56ee3
Jacqueline <jacq@in-the-box.nl>
2017-02-22 12:03:18 +0100

    Update install.ps1

--- extras/w10-devmachine/install.ps1
+++ extras/w10-devmachine/install.ps1
@@ -10,4 +10,3 @@ choco install git-credential-winstore --yes --force
 choco install conemu --yes --force
 choco install googlechrome --yes --force
 choco install visualstudiocode --yes --force
-choco install VisualStudio2015Community --yes --force --packageParameters "--AdminFile https://gist.githubusercontent.com/jacqinthebox/494111cc5e2941e963d1424d15813cf2/raw/2a23194c41d866a7cd8ad8d68efe8a82595dfa70/AdminDeployment.xml"
