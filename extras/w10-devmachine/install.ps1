27c6261de7fe8c912e4e6ee329a1f0ed98fbca6d
Jacqueline <jacq@in-the-box.nl>
2017-03-15 21:37:42 +0100

    Update install.ps1

--- extras/w10-devmachine/install.ps1
+++ extras/w10-devmachine/install.ps1
@@ -1,6 +1,6 @@
 #New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
 New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 1
-Install-Module ISESteroids -Scope CurrentUser -Force
+Install-Module ISESteroids -Scope CurrentUser
 
 choco install git --yes --force
 choco install git-credential-winstore --yes --force
