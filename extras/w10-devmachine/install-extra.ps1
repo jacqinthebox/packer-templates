10665a72d919789b97d7cd3cae9da5f560aa6637
Jacqueline <jacq@in-the-box.nl>
2017-07-30 11:31:29 +0200

    Update install-extra.ps1

--- extras/w10-devmachine/install-extra.ps1
+++ extras/w10-devmachine/install-extra.ps1
@@ -1,10 +1,11 @@
 # Script to bootstrap a Vagrant Win10 development box
 
 param(
+    [Parameter(Mandatory = $true)]
     [ValidateSet('small','medium','large','x-large')]
     [string]$installtype,
     [Parameter(Mandatory = $true)]
-    [bool]$vm = $True
+    [bool]$virtualmachine
 )
 
 Set-ExecutionPolicy Bypass
@@ -13,6 +14,15 @@ New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Expl
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 Install-PackageProvider -Name Nuget -Force
+
+Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/presentations/master/Microsoft.PowerShell_profile.ps1 -OutFile $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Invoke-Webrequest https://raw.githubusercontent.com/jacqinthebox/presentations/master/Microsoft.PowerShellISE_profile.ps1 -OutFile $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
+
+choco install visualstudiocode -force -yes
+choco install googlechrome -force -yes
+choco install git -force -yes
+choco install conemu -force -yes
+choco install notepadplusplus -force -yes
 
 choco install notepadplusplus -force -yes
 choco install 7zip.install  -force -yes
@@ -43,11 +53,14 @@ if ($installtype -eq  'x-large') {
     choco install sql-server-management-studio -force -yes
 }
 
-if ($vm -eq $false) {
+if ($virtualmachine -eq $false) {
    choco install dropbox -force -yes
    choco install googledrive -force -yes
    choco install virtualbox -force -yes
    choco install vagrant -force -yes
+   choco install vlc -force -yes
 }
 
+
+Set-ExecutionPolicy restricted
 #choco upgrade all
