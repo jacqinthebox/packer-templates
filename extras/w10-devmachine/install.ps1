8065828a1f9753f4ff424b17da1037f335390401
jacqueline <jacq@in-the-box.nl>
2017-09-17 09:25:32 +0200

    improved devmachine example

--- extras/w10-devmachine/install.ps1
+++
@@ -1,9 +0,0 @@
-#New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
-New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 1
-Install-Module ISESteroids -Scope CurrentUser
-
-choco install git --yes --force
-choco install git-credential-winstore --yes --force
-choco install conemu --yes --force
-choco install googlechrome --yes --force
-choco install visualstudiocode --yes --force
