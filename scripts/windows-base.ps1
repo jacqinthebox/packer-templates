930b6a5a76900ae0c10b3daefedc0094b5c45a53
Jacqueline <jacq@in-the-box.nl>
2017-09-17 08:37:55 +0200

    Added DisableFirstRunWizard registry setting

--- scripts/windows-base.ps1
+++ scripts/windows-base.ps1
@@ -4,3 +4,5 @@ netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 
 reg add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server' /v fDenyTSConnections /t REG_DWORD /d 0 /f
 Set-ItemProperty -Path 'HKLM:\SYSTEM\Setup\Status\SysprepStatus'  -Name  'GeneralizationState' -Value 7
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
+Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\UnattendBackup\ActiveSetup\DisableFirstRunWizard" -Name DisableFirstRunWizard -Value 1
+
