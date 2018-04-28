bc7d2aa32839987f2b1b280a3ce0fc047bfad235
Jacqueline <jacq@in-the-box.nl>
2018-04-28 13:33:00 +0200

    Update install-extra.ps1

--- extras/devmachine/install-extra.ps1
+++ extras/devmachine/install-extra.ps1
@@ -1,33 +1,20 @@
-Function Add-Software {
-
-param(
-    [Parameter(Mandatory = $True)]
-    [bool]$InstallVisualStudio
-)
-
-Set-ExecutionPolicy Bypass
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 Install-PackageProvider -Name Nuget -Force
 
-choco install visualstudiocode -force -yes
-choco install git -force -yes
-choco install notepadplusplus -force -yes
+$software = @(
+    'visualstudiocode'
+    'git',
+    'conemu',
+    'googlechrome',
+    'notepadplusplus'
+)
 
-choco install 7zip.install  -force -yes
-choco install 7zip.commandline -force -yes
-choco install firefox -force -yes
-choco install git -force -yes
-choco install sumatrapdf -force -yes
-choco install sumatrapdf.install -force -yes
-choco install conemu -force -yes
 
-Install-Module Posh-Git -Force
-Install-Module Oh-My-Posh -Force
-Install-Module ISESteroids -Scope CurrentUser -Force
-
-if ( $InstallVisualStudio -eq $True ) {
-    choco install visualstudio2017enterprise  -force -yes
-    choco install sql-server-management-studio -force -yes
-    }
+foreach ($s in  $software) {
+    choco install $s -yes
 }
+
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Module oh-my-posh -Scope CurrentUser -Force
+
+Invoke-WebRequest https://github.com/jacqinthebox/packer-templates/blob/master/extras/devmachine/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.ttf?raw=true -OutFile ~\Desktop\Meslo.ttf
