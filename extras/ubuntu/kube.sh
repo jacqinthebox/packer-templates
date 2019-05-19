cee1bc601500a49da4f1bb60a698bdb2a0efdaa2
Jacqueline <jacq@in-the-box.nl>
2019-05-19 09:34:59 +0200

    Update kube.sh

--- extras/ubuntu/kube.sh
+++ extras/ubuntu/kube.sh
@@ -1,6 +1,8 @@
-
+#! /bin/bash
 apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
+
+curl -L https://git.io/get_helm.sh | bash
