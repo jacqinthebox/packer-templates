fad5154e69b2d448d6aea4e18bb2eab575bac53e
Jacqueline <jacq@in-the-box.nl>
2019-05-19 09:33:34 +0200

    Create kube.sh

---
+++ extras/ubuntu/kube.sh
@@ -0,0 +1,6 @@
+
+apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
